90c46091db5cde81bb872b0d3859cb9772d58deb
fakorede <abiolafakorede@gmail.com>
2019-10-26 03:15:02 +0100

    defining one to many relations

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class BlogPost extends Model
 {
     protected $fillable = ['title', 'content'];
+
+    public function comment()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
