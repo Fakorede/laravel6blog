161b2ab64a2dadc2e451c3d710e8e4d27f597797
fakorede <abiolafakorede@gmail.com>
2019-11-28 21:16:47 +0100

    creating reusable laravel components

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,15 +16,18 @@
                         href="{{ route('posts.show', [ 'post' => $post->id ]) }}">{{ $post->title }}</a>
 
                         @if ($post->trashed())
-                            <del>
+                        </del>
                         @endif
 
                     </h3>
 
-                    <p class="text-muted">
+                    {{-- <p class="text-muted">
                         Added: {{ $post->created_at->diffForHumans() }} 
                         by {{ $post->user->name }}
-                    </p>
+                    </p> --}}
+
+                    @updated(['date' => $post->created_at, 'name' => $post->user->name])
+                    @endupdated
                     
     
                     @if($post->comments_count)
@@ -56,12 +59,11 @@
         <div class="col-4">
             <div class="container">
                 <div class="row">
-                    <div class="card" style="width: 100%;">
-                        <div class="card-body">
-                            <h5 class="card-title">Most Commented</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">What people are currently talking about</h6>
-                        </div>
-                        <ul class="list-group list-group-flush">
+                    @card(['title' => 'Most Commented'])
+                        @slot('subtitle')
+                            What people are currently talking about 
+                        @endslot
+                        @slot('items')
                             @foreach ($mostCommented as $post)
                                 <li class="list-group-item">
                                     <a href="{{ route('posts.show', ['post' => $post->id]) }}">
@@ -69,38 +71,24 @@
                                     </a>
                                 </li>
                             @endforeach
-                        </ul>
-                    </div>
+                        @endslot
+                    @endcard
                 </div> 
                 <div class="row mt-4">
-                    <div class="card" style="width: 100%;">
-                        <div class="card-body">
-                            <h5 class="card-title">Most Active Users</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Users with most posts</h6>
-                        </div>
-                        <ul class="list-group list-group-flush">
-                            @foreach ($mostActive as $user)
-                                <li class="list-group-item">
-                                    {{ $user->name }}
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
+                    @card(['title' => 'Most Active Users'])
+                        @slot('subtitle')
+                            Users with most written posts
+                        @endslot
+                        @slot('items', collect($mostActive)->pluck('name'))
+                    @endcard
                 </div> 
                 <div class="row mt-4">
-                    <div class="card" style="width: 100%;">
-                        <div class="card-body">
-                            <h5 class="card-title">Most Active Last Month</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Users with most posts created in the last month</h6>
-                        </div>
-                        <ul class="list-group list-group-flush">
-                            @foreach ($mostActiveLastMonth as $user)
-                                <li class="list-group-item">
-                                    {{ $user->name }}
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
+                    @card(['title' => 'Most Active Last Month'])
+                        @slot('subtitle')
+                            Users with most posts created in the last month
+                        @endslot
+                        @slot('items', collect($mostActiveLastMonth)->pluck('name'))
+                    @endcard
                 </div> 
             </div>
         </div>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,20 +1,29 @@
 @extends('layout')
 
 @section('content')
-    <h1>{{ $post->title }}</h1>
-    <p>{{ $post->content }}</p>
-    <p>Added: {{ $post->created_at->diffForHumans() }}</p>
+    <h1>
+        {{ $post->title }}
 
-    @if((new Carbon\Carbon())->diffInMinutes($post->created_at) < 30)
-        @badge(['type' => 'primary'])
+        @badge(['show' => now()->diffInMinutes($post->created_at) < 30])
             New
         @endbadge
-    @endif
+
+    </h1>
+    <p>{{ $post->content }}</p>
+    {{-- <p>Added: {{ $post->created_at->diffForHumans() }}</p> --}}
+    @updated(['date' => $post->created_at, 'name' => $post->user->name])
+    @endupdated
+    @updated(['date' => $post->updated_at])
+        Updated
+    @endupdated
 
     <h3>Comments</h3>
     @forelse($post->comments as $comment)
         <p>
-            {{ $comment->content }}<sup class="text-muted"> added {{ $comment->created_at->diffForHumans() }}</sup> 
+            {{ $comment->content }}
+            {{-- <sup class="text-muted"> added {{ $comment->created_at->diffForHumans() }}</sup> --}}
+            @updated(['date' => $comment->created_at])
+            @endupdated 
         </p>
     @empty
         <p>No comments yet</p>
